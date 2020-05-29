5f609b98df4b3354ee208f5b51701523cad24661
shaurya31 <shaurya.gomber98@iitg.ernet.in>
2020-05-30 00:55:24 +0530

    Implement the Luby Sequence Generator file

---
+++ LubyGenerator.py
@@ -0,0 +1,31 @@
+import math
+
+l=[]
+mult = 1
+minu = 0
+
+def get_next():
+    global l
+    global mult
+    global minu
+
+    size = len(l)
+    to_fill = size+1
+
+    if math.log(to_fill+1,2).is_integer():
+        l.append(mult)
+        minu = size+1
+        mult *= 2
+    else:
+        l.append(l[to_fill-minu-1])
+    
+    return l[size]
+
+def reset():
+    global l
+    global mult
+    global minu
+
+    l=[]
+    mult=1
+    minu=0
